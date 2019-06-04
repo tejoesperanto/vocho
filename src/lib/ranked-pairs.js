ade025502ce797b0e5bf913b4665e209856b42ab
Mia Nordentoft <mia@miestasmia.com>
2019-06-04 13:50:23 +0200

    Added documentation to ranked pairs

--- src/lib/ranked-pairs.js
+++ src/lib/ranked-pairs.js
@@ -30,6 +30,14 @@ function isCyclic (graph) {
 	return false;
 }
 
+/**
+ * Runs a tideman ranked pairs election
+ * @param {string[]|string} candidates    The candidates. Each candidate must be represented by one character
+ * @param {string[]} ballots              All ballots written using tideman ranked pairs syntax
+ * @param {string[]}  [ignoredCandidates] An array of candidates to ignore
+ * @param {string} tieBreaker             The fully inclusive tie breaker ballot without any equals
+ * @return {string} The winning candidate
+ */
 function RankedPairs (candidates, ballots, ignoredCandidates = [], tieBreaker) {
 	if (typeof candidates === 'string') { candidates = candidates.split(''); }
 	candidates.sort();

135d38aab4bd484346402e9bed153037ef291770
Mia Nordentoft <mia@miestasmia.com>
2019-06-05 17:59:26 +0200

    Experimental new build system

--- build.sh
+++ build.sh
@@ -7,8 +7,14 @@ mkdir -p node
 
 # Linux x64
 if [ ! -d "node/node-v$VERSION-linux-x64" ]; then
-	echo Downloading linux-x64 ...
+	echo Downloading node linux-x64 ...
 	wget -qO- "https://nodejs.org/dist/v$VERSION/node-v$VERSION-linux-x64.tar.xz" | tar -xJf - -C node
+fi
+
+# Darwin x64
+if [ ! -d "node/node-v$VERSION-darwin-x64" ]; then
+	echo Downloading node darwin-x64 ...
+	wget -qO- "https://nodejs.org/dist/v$VERSION/node-v$VERSION-darwin-x64.tar.gz" | tar -xzf - -C node
 fi
 
 
@@ -18,11 +24,15 @@ fi
 echo Creating linux-x64 files ...
 mkdir -p linux-x64 && cd $_
 cp -r "../node/node-v$VERSION-linux-x64" node
-rsync -a ../../ vocho --exclude dist --exclude .git
+rsync -a ../../ vocho --exclude dist --exclude .git --exclude node_modules
 cat >run.sh <<EOL
 #!/usr/bin/env bash
 cd \`dirname \$0\`
-node/bin/node vocho/src/index.js
+cd vocho
+if [ ! -d node_modules ]; then
+	../node/bin/npm i
+fi
+../node/bin/node src/index.js
 EOL
 chmod +x run.sh
 
@@ -30,3 +40,24 @@ echo Tarballing linux-x64 ...
 tar -cJf ../linux-x64.tar.xz *
 echo Cleaning up from linux-x64
 cd .. && rm -rf linux-x64
+
+# Darwin x64
+echo Creating darwin-x64 files ...
+mkdir -p darwin-x64 && cd $_
+cp -r "../node/node-v$VERSION-darwin-x64" node
+rsync -a ../../ vocho --exclude dist --exclude .git --exclude node_modules
+cat >run.sh <<EOL
+#!/usr/bin/env bash
+cd \`dirname \$0\`
+cd vocho
+if [ ! -d node_modules ]; then
+	../node/bin/npm i
+fi
+../node/bin/node src/index.js
+EOL
+chmod +x run.sh
+
+echo Tarballing darwin-x64 ...
+tar -cJf ../darwin-x64.tar.xz *
+echo Cleaning up from darwin-x64
+cd .. && rm -rf darwin-x64
